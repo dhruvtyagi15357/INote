c5a70eb8cf5e0ac29366bc4b38d4c0a7213cc552
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-02-05 16:19:40 +0530

    created login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken")
 const JWT_SECRET = "Dhruv is a good $oy"
 
 //create a user using: POST "/api/auth/createuser"  no login required
-router.post('/createuser', [
+router.post('/register', [
     body('email', "Enter valid email!").isEmail(),
     body('name').isLength({min:2, max:50}),
     body('password').isLength({min:5})
@@ -45,11 +45,50 @@ router.post('/createuser', [
 
 
 
-        
+
     } catch (err){
         // console.log(err)
         res.status(500).send(err.message)
     }
 })
 
+
+
+
+
+//create a user using: POST "/api/auth/createuser"  no login required
+router.post('/login', [
+    body('email', "Enter valid email!").isEmail(),
+    body('password', "Password cannot be blank").exists(),
+],async (req, res) =>{
+    const email = req.body.email
+    const password = req.body.password
+
+
+    try{
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error: 'sorry, user does not exist'})
+        }
+        const passwordCmp = await bcrypt.compare(password, user.password)
+        if(!passwordCmp){
+            return res.status(400).json({error: 'sorry, wrong password'})
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        
+        const authToken = jwt.sign(data, JWT_SECRET)
+        res.json({authToken})
+
+    } catch (err) {
+        res.status(500).send(err.message)
+        console.log(err)
+
+    }
+
+})
 module.exports = router
