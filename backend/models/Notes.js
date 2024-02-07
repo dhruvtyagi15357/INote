10f1f2e847160e337e827175b624ce9b7c7580e2
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-02-07 07:19:21 +0530

    Add user field to Notes model and implement CRUD operations for notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true,
     },
 
-    desc:{
+    description:{
         type: String,
         required: true
     },
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,80 @@
 const express = require('express')
 const router = express.Router()
+const fetchuser = require("../middleware/fetchuser")
+const Notes = require('../models/Notes')
+const { body, validationResult } = require('express-validator')
 
-router.get('/', (req, res) =>{
-    console.log(req.body)
-    res.send('hello')
+
+//Route 1: Get all the notes
+router.get('/fetchallnotes', fetchuser, async (req, res) =>{
+    const notes = await Notes.find({user: req.user.id})
+    res.json(notes)
+})
+
+//Route 2: Add a new note using post
+router.post('/addnote', fetchuser, [
+    body('title', 'Title length should be atleast 3').isLength({min:3}),
+    body('description', 'Description length should be atleast 5').isLength({min:5})
+], async (req, res) =>{
+    try{
+        const error = validationResult(req);
+        if (!error.isEmpty()){
+            return res.status(400).json({error:error.array()})
+        }
+
+        const note = await Notes.create({
+            user: req.user.id,
+            title: req.body.title,
+            description: req.body.description,
+            tag:req.body.tag,
+        })
+
+        const savednote = await note.save()
+        res.json(savednote)
+    } catch (err) {
+        res.status(500).send("Internal server error")
+        console.log(err)
+    }    
+
+})
+
+
+router.put('/updatenote/:id', fetchuser, 
+// [
+//     body('title', 'Title length should be atleast 3').isLength({min:3}),
+//     body('description', 'Description length should be atleast 5').isLength({min:5})
+// ],
+async (req, res) =>{
+    try{
+        const {title, description, tag} = req.body
+        // create new note object
+        const newNote = {}
+        if(title){
+            newNote.title = title
+        }
+        if(description){
+            newNote.description = description
+        }
+        if(tag){
+            newNote.tag = tag
+        }
+
+
+        // find the node to be updated and update it.
+        var note = Notes.findOne({_id: req.params.id, user: req.user.id})
+        if(!note){
+            res.status('404').send('No such note found')
+            return
+        }
+        
+
+        note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+        res.json(note)
+
+    } catch (err) {
+        res.status(500).send("Internal server error")
+        console.log(err)
+    }
 })
 
 module.exports = router
