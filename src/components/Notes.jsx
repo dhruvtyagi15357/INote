851161c7a46f0505c77520377c78f9f4c33f8012
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-03-10 15:15:21 +0530

    wow2

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useRef, useState } from "react";
-import noteContext from "../context/notes/notecontext";
+import noteContext from "../context/notes/noteContext";
 import NoteItem from "./NoteItem";
 import AddNote from "./AddNote";
 import { useNavigate } from "react-router-dom";
